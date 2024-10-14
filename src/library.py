f66d8c5e56dd93729a78c8484ef2e05ee173a062
Сергей <osedchii.serega48@gmail.com>
2024-10-14 14:42:26 +0300

    class Library v2

--- src/library.py
+++ src/library.py
@@ -1,7 +1,23 @@
 from dataclasses import dataclass
 
+from author_iterator import AuthorIterator
+from library_iterator import LibraryIterator
+from genre_iterator import GenreIterator
+
 
 @dataclass
 class Library:
 
     books = []
+
+    def add_book(self, book: str):
+        self.books.append(book)
+
+    def get_author_iterator(self):
+        return AuthorIterator(self)
+
+    def get_genre_iterator(self):
+        return GenreIterator(self)
+
+    def __iter__(self):
+        return LibraryIterator(self)
